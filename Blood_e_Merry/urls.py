3ab3c4039133662d4cf0a73c2f35c51f3bd74643
Visesh <viseshrprasad@gmail.com>
2017-04-06 19:50:11 -0400

    Define URLs

--- Blood_e_Merry/urls.py
+++ Blood_e_Merry/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import reverse_lazy
+from django.views.generic import RedirectView
+
+# from loginsignup import views
+#
+# # above import might not work
+from Blood_e_Merry import views
 
 urlpatterns = [
+    url(r'^$', views.redirect_home, name='redirect_home'),  # actual home page
+    url(r'^home/', include('loginsignup.urls')),
     url(r'^admin/', admin.site.urls),
 ]
