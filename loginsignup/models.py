06bdb8f99b26a8e4b5c4054e79f38b3dd86822bc
Visesh <viseshrprasad@gmail.com>
2017-04-06 19:50:31 -0400

    add custom model

--- loginsignup/models.py
+++ loginsignup/models.py
@@ -1,3 +1,37 @@
-from django.db import models
+from datetime import datetime
 
-# Create your models here.
+from django.db.models.signals import post_save
+from django.utils.encoding import python_2_unicode_compatible
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+@python_2_unicode_compatible
+class Donor(models.Model):
+    user = models.OneToOneField(User)
+    phone = models.CharField(max_length=10, default='')
+    city = models.CharField(max_length=10, default='')
+    state = models.CharField(max_length=10, default='')
+    country = models.CharField(max_length=30, default='')
+    reg_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    # def __str__(self):
+    #     return self.email
+
+    def was_reg_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.reg_date <= now
+
+    was_reg_recently.admin_order_field = 'reg_date'
+    was_reg_recently.boolean = True
+    was_reg_recently.short_description = 'Registered recently?'
+
+
+def create_profile(sender, **kwargs):
+    if kwargs['created']:
+        user_profile = Donor.objects.create(user=kwargs['instance'])
+
+
+post_save.connect(create_profile, sender=User)
+# use post_save to trigger tweepy later
